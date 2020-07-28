977817935720c72c8cf7588db50769a047ae9e38
Alvaro García Sabuco <alvarogs56@gmail.com>
2020-07-28 18:34:16 +0200

    user login

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@
 var validator = require('validator');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
+var jwt = require('../services/jwt');
 
 var controller = {
 
@@ -85,6 +86,70 @@ var controller = {
                 message: "Validación de los datos del usuario incorrecta, inténtalo de nuevo",
             });
         }
+    },
+
+    login: function(req, res){
+        //Recoger parametros de la peticion
+        var params = req.body;
+
+        //Validar los datos
+        var validate_email = !validator.isEmpty(params.email) && validator.isEmail(params.email);
+        var validate_password = !validator.isEmpty(params.password);
+
+        if(!validate_email || !validate_password) {
+            return res.status(400).send({
+                message: 'Los datos son incorrectos'
+            });
+        }
+
+        //Buscar usuarios que coincidan con el mail
+        User.findOne({email: params.email.toLowerCase()}, (err, user) => {
+            //Si lo encuentra,
+            if(err) {
+                return res.status(500).send({
+                    message: 'Error al intentar identificarse'
+                });
+            }
+
+            if(!user) {
+                return res.status(404).send({
+                    message: 'El usuario no existe'
+                });
+            }
+
+            //Comprobar la contraseña
+            bcrypt.compare(params.password, user.password, (err, isLogged) => {
+
+                if(isLogged) {
+                    //Generar token de JWT y devolverlo
+                    if(params.getToken) {
+
+                        return res.status(200).send({
+                            token: jwt.createToken(user)
+                        });
+
+                    } else {
+                        //Limpiar objeto
+                        user.password = undefined;
+
+                        //Devolver datos
+                        return res.status(200).send({
+                            status: 'success',
+                            user
+                        });
+                    }
+
+                } else {
+
+                    return res.status(200).send({
+                        message: "Las credenciales no son correctas"
+                    });
+                    
+                }
+            });
+        });
+
+        
     }
 };
 
